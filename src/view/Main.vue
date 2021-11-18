bc9da05e51bf2bae6462a6b6dc0670525f3b2bfc
frank122080 <frankchang122080@gmail.com>
2021-11-18 22:23:24 +0800

    重新建專安

---
+++ src/view/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <Input clearable>
+        <Button slot="append" type="primary"></Button>新增</Button>
+      </Input>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
